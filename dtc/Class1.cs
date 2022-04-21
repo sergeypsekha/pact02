6353b2846bbfa65a19f98ab7043d1f0bc0da788c
SP <sergey.psekha@gmail.com>
2022-04-21 09:00:19 +0200

    added dtc projects

---
+++ dtc/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace dtc;
+public class Class1
+{
+
+}
---
+++ dtc/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dtc
+{
+    public class Main
+    {
+        public void EntryPoint(string[] args)
+        {
+            throw new NotImplementedException("Not implemented");
+        }
+    }
+}
