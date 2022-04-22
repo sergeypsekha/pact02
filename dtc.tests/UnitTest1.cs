15317a132501eb0c086f2a9f56134789ce0cdedb
SP <sergey.psekha@gmail.com>
2022-04-22 11:15:46 +0200

    added pact

--- dtc.tests/UnitTest1.cs
+++ dtc.tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using PactNet.Matchers;
+using System.Dynamic;
 using Xunit;
 
 namespace dtc.tests;
@@ -7,6 +9,16 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        var obj = new Customer()
+        {
+            Id = 0,
+            Name = "Wolf",
+            Dob = "11/01/2018"
+        };
 
+        dynamic dObj = new ExpandoObject();
+        dObj.id = Match.Type(obj.Id);
+        dObj.Name = obj.Name;
+        dObj.Dob = Match.Type(obj.Dob);
     }
 }
---
+++ dtc/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace dtc
+{
+    public class Customer
+    {
+        public Customer()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Dob { get; set; }
+    }
+}
